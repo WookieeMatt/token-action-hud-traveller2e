78bc7e176880202ea8d934fbb465d874d1bb7d30
WookieeMatt <mrimler1983@gmail.com>
2025-02-15 10:53:40 -0700

    first commit

---
+++ scripts/init.js
@@ -0,0 +1,13 @@
+import { SystemManager } from './system-manager.js'
+import { MODULE, REQUIRED_CORE_MODULE_VERSION } from './constants.js'
+
+Hooks.on('tokenActionHudCoreApiReady', async () => {
+    /**
+     * Return the SystemManager and requiredCoreModuleVersion to Token Action HUD Core
+     */
+    const module = game.modules.get(MODULE.ID)
+    module.api = {
+        SystemManager
+    }
+    Hooks.call('tokenActionHudSystemReady', module)
+})
---
+++ scripts/settings.js
@@ -0,0 +1,31 @@
+import { MODULE } from "./constants.js";
+
+/**
+ * Register module settings
+ * Called by Token Action HUD Core to register Token Action HUD system module settings
+ * @param {function} coreUpdate Token Action HUD Core update function
+ */
+export function register(coreUpdate) {
+	game.settings.register(MODULE.ID, "displayUnequipped", {
+		name: game.i18n.localize("tokenActionHud.settings.displayUnequipped.name"),
+		hint: game.i18n.localize("tokenActionHud.settings.displayUnequipped.hint"),
+		scope: "client",
+		config: true,
+		type: Boolean,
+		default: false,
+		onChange: (value) => {
+			coreUpdate(value);
+		},
+	});
+	// game.settings.register(MODULE.ID, "showtooltip", {
+	// 	name: game.i18n.localize("ALIENRPG.TAH-tooltip-name"),
+	// 	hint: game.i18n.localize("ALIENRPG.TAH-tooltip-hint"),
+	// 	scope: "client",
+	// 	config: true,
+	// 	type: Boolean,
+	// 	default: true,
+	// 	onChange: (value) => {
+	// 		coreUpdate(value);
+	// 	},
+	// });
+}
