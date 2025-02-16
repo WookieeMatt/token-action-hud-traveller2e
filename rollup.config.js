f85753642ed49c7695e6b898d51e81d1a20b9ad1
TarcisMoreda <tarcismoreda@gmail.com>
2025-02-16 15:10:33 -0300

    feat: Added rollup and linter

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import terser from '@rollup/plugin-terser'
+import multi from '@rollup/plugin-multi-entry'
+
+export default [
+    {
+        input: {
+            include: [
+                'scripts/*.js'
+            ],
+            exclude: [
+                'scripts/token-action-hud-t2e.min.js']
+        },
+        output: {
+            format: 'esm',
+            file: 'scripts/token-action-hud-t2e.min.js',
+            sourcemap: true
+        },
+        plugins: [
+            terser({ keep_classnames: true, keep_fnames: true }),
+            multi()
+        ]
+    }
+]
